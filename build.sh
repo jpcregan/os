59245d0b3d73ca2e24b6f0adf942dbd0e58ff3bd
Jordan Cregan <jpcregan@protonmail.com>
2025-10-26 21:30:00 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 hex src/boot.hex boot.bin 
 hex src/kernel.hex kernel.bin
 
-dd if=boot.bin of=os.img status=none
+dd if=boot.bin of=os.img bs=512 seek=0 status=none
 dd if=kernel.bin of=os.img bs=512 seek=1 status=none
