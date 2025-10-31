2557a2e781c375aebb3db045485141b7b88783f1
Jordan Cregan <jpcregan@protonmail.com>
2025-10-31 15:40:56 +0000

    Remove duplicate space from build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ hex src/boot.hex boot.bin
 hex src/kernel.hex kernel.bin
 hex src/shell.hex shell.bin
 
-
 dd if=boot.bin of=os.img bs=512 seek=0 status=none
 dd if=kernel.bin of=os.img bs=512 seek=1 status=none
 dd if=shell.bin of=os.img bs=512 seek=8 status=none
