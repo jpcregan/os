379333537a35ca062324d78c8d6c529e047d93c0
Jordan Cregan <jpcregan@protonmail.com>
2025-10-31 15:39:37 +0000

    Initial program loading support

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 hex src/boot.hex boot.bin 
 hex src/kernel.hex kernel.bin
+hex src/shell.hex shell.bin
+
 
 dd if=boot.bin of=os.img bs=512 seek=0 status=none
 dd if=kernel.bin of=os.img bs=512 seek=1 status=none
+dd if=shell.bin of=os.img bs=512 seek=8 status=none
